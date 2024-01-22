cb5482f44326a0191d3c20d39b7caa74be156551
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-01-22 14:06:07 +0100

    Update dripcontent-wpdv.js

--- dripcontent-wpdv.js
+++ dripcontent-wpdv.js
@@ -10,59 +10,66 @@ window.addEventListener('load', function() {
   function checkMemberPlan() {
     var userData = JSON.parse(localStorage.getItem('_ms-mem'));
     console.log('User data:', userData);
-    var allItemsActive = true; // Variable pour suivre si tous les éléments sont actifs
 
-    if (userData && userData.metaData && userData.metaData.start_date_wf_wpdv && userData.planConnections) {
-      var validPlanIds = ["pln_formation-webflow-page-de-vente-3-fois--ul110zw2"]; // Remplacez avec vos plans IDs en tableau si plusieurs
+    if (userData && userData.metaData && userData.metaData.start_date_wf_eco && userData.planConnections) {
+      var validPlanIds = ["pln_formation-webflow-page-de-vente-3-fois--ul110zw2", "pln_coriace-formation-webflow-page-de-vente-6b4m0150"]; // Ajout du plan spécifique
       
       var hasRequiredPlan = userData.planConnections.some(function(plan) {
         return validPlanIds.includes(plan.planId) && plan.status === "ACTIVE";
       });
 
+      var hasSpecialPlan = userData.planConnections.some(function(plan) {
+        return plan.planId === "pln_formation-webflow-e-commerce-cms-kb40awg" && plan.status === "ACTIVE";
+      });
+
       if (hasRequiredPlan) {
-        var startDate = new Date(userData.metaData.start_date_wf_wpdv);
+        var startDate = new Date(userData.metaData.start_date_wf_eco);
         var daysForLevel2 = 30; // Nombre de jours après lesquels le niveau 2 est disponible
         var daysForLevel3 = 60; // Nombre de jours après lesquels le niveau 3 est disponible
         var now = new Date();
         var daysSinceStart = Math.floor((now - startDate) / (1000 * 60 * 60 * 24));
-        
+
         var accessLevel = 1; // Définir le niveau d'accès initial
         if (daysSinceStart >= daysForLevel2) { accessLevel = 2; }
         if (daysSinceStart >= daysForLevel3) { accessLevel = 3; }
 
-        // Mettre à jour les éléments span avec le temps restant pour les niveaux 2 et 3
-        var timeLeftSpanLevel2 = document.getElementById('courseTimeLeft2'); // ID pour le niveau 2
-        var timeLeftSpanLevel3 = document.getElementById('courseTimeLeft3'); // ID pour le niveau 3
+        var timeLeftSpanLevel2 = document.getElementById('courseTimeLeft2');
+        var timeLeftSpanLevel3 = document.getElementById('courseTimeLeft3');
         var timeLeftForLevel2 = calculateDaysLeft(startDate, daysForLevel2);
         var timeLeftForLevel3 = calculateDaysLeft(startDate, daysForLevel3);
 
         if (timeLeftSpanLevel2) timeLeftSpanLevel2.textContent = timeLeftForLevel2 + " jours";
         if (timeLeftSpanLevel3) timeLeftSpanLevel3.textContent = timeLeftForLevel3 + " jours";
 
+        var courseTimeLeftCard2 = document.getElementById('courseTimeLeftCard2');
+        var courseTimeLeftPrice = document.getElementById('courseTimeLeftPrice');
+        var courseTimeLeftButton = document.getElementById('courseTimeLeftButton');
+
+        if (daysSinceStart >= daysForLevel2) {
+          if (courseTimeLeftCard2) courseTimeLeftCard2.style.display = 'none';
+          if (courseTimeLeftPrice) courseTimeLeftPrice.textContent = "60€";
+          if (courseTimeLeftButton) courseTimeLeftButton.href = "https://order.coriace.co/formation/commande-webflow-page-de-vente-60/etape/commande-webflow-page-de-vente-60/";
+        } else {
+          if (courseTimeLeftPrice) courseTimeLeftPrice.textContent = "120€";
+          if (courseTimeLeftButton) courseTimeLeftButton.href = "https://order.coriace.co/formation/commande-webflow-page-de-vente-120/etape/commande-webflow-page-de-vente-120/";
+        }
+
         document.querySelectorAll('.course_lesson-item').forEach(function(item) {
           var paidId = parseInt(item.getAttribute('data-paid-id'), 10);
           var lessonMask = item.querySelector('.course_lesson-mask');
 
-          if (paidId > accessLevel) {
-            allItemsActive = false; // Indiquer qu'il y a au moins un élément inactif
-            item.style.opacity = '0.5'; // Griser l'item
-            if (lessonMask) {
-              lessonMask.style.display = 'block'; // Afficher le masque
-            }
+          if (paidId > accessLevel && !hasSpecialPlan) {
+            item.style.opacity = '0.5';
+            if (lessonMask) lessonMask.style.display = 'block';
           } else {
-            item.style.opacity = '1'; // Restaurer l'opacité normale
-            if (lessonMask) {
-              lessonMask.style.display = 'none'; // Masquer le masque
-            }
+            item.style.opacity = '1';
+            if (lessonMask) lessonMask.style.display = 'none';
           }
         });
 
-        var courseNavigation = document.getElementById('courseNavigationWpdv');
-        if (!allItemsActive && courseNavigation) {
-          courseNavigation.style.display = 'none';
-        } else if (courseNavigation) {
-          courseNavigation.style.display = 'flex';
-        }
+        var courseNavigation = document.getElementById('courseNavigationEco');
+        if (!allItemsActive && courseNavigation) courseNavigation.style.display = 'none';
+        else if (courseNavigation) courseNavigation.style.display = 'flex';
       } else {
         console.log("L'utilisateur n'est pas sur un des plans requis pour ce contenu.");
       }
