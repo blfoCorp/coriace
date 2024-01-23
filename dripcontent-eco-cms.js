e88f916567695acb39d7b0fb80b76e23a49a7efc
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-01-23 14:34:39 +0100

    Update dripcontent-eco-cms.js

--- dripcontent-eco-cms.js
+++ dripcontent-eco-cms.js
@@ -48,12 +48,12 @@ window.addEventListener('load', function() {
       if (daysSinceStart >= daysForLevel2) {
         if (courseTimeLeftCard2) courseTimeLeftCard2.style.display = 'none';
         if (courseTimeLeftPrice && userData.metaData.start_date_wf_eco) courseTimeLeftPrice.textContent = "60€";
-        if (courseTimeLeftPrice && userData.metaData.start_date_wf_pack) courseTimeLeftPrice.textContent = "60€";
+        if (courseTimeLeftPrice && userData.metaData.start_date_wf_pack) courseTimeLeftPrice.textContent = "180€";
         if (courseTimeLeftButton && userData.metaData.start_date_wf_eco) courseTimeLeftButton.href = "https://order.coriace.co/formation/commande-webflow-ecommerce-cms-60/etape/commande-webflow-ecommerce-cms-60/";
         if (courseTimeLeftButton && userData.metaData.start_date_wf_pack) courseTimeLeftButton.href = "https://coriace.co/60";
       } else {
         if (courseTimeLeftPrice && userData.metaData.start_date_wf_eco) courseTimeLeftPrice.textContent = "120€";
-        if (courseTimeLeftPrice && userData.metaData.start_date_wf_pack) courseTimeLeftPrice.textContent = "120€";
+        if (courseTimeLeftPrice && userData.metaData.start_date_wf_pack) courseTimeLeftPrice.textContent = "360€";
         if (courseTimeLeftButton && userData.metaData.start_date_wf_eco) courseTimeLeftButton.href = "https://order.coriace.co/formation/commande-webflow-ecommerce-cms-120/etape/commande-webflow-ecommerce-cms-120/";
         if (courseTimeLeftButton && userData.metaData.start_date_wf_pack) courseTimeLeftButton.href = "https://coriace.co/120";
       }
