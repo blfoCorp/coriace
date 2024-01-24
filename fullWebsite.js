2524ef2981ffcc0d6d0676d1a1b81ef310c73aec
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-01-24 15:00:08 +0100

    Update fullWebsite.js

--- fullWebsite.js
+++ fullWebsite.js
@@ -1,4 +1,5 @@
 /*---  DÉBUT : DÉVEROUILLAGE FORMATION EN FONCTION DU PLAN DANS LA NAVIGATION ----*/
+
 async function updateTabLinksAndHideElementsForSpecificPlans() {
     const response = await window.$memberstackDom.getCurrentMember();
     const member = response.data;
