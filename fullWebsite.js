93d82da9589b54def9387fbff67698d7df841f97
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-01-25 14:56:33 +0100

    Update fullWebsite.js

--- fullWebsite.js
+++ fullWebsite.js
@@ -1,3 +1,35 @@
+/*---  DÉBUT : BLOCAGE CLIC DROIT ----*/
+
+// Bloquer le clic droit
+document.addEventListener('contextmenu', function (event) {
+ event.preventDefault();
+  alert("Curieux de connaître ce qui se cache derrière notre espace membre ? Découvre tout cela dans nos formations avancées 😉");
+});
+
+// Bloquer les raccourcis clavier
+document.addEventListener('keydown', function (event) {
+  // Bloquer les raccourcis courants pour accéder au panneau de développement
+  const isF12 = event.keyCode === 123;
+  const macShortcutJ = event.metaKey && event.altKey && event.keyCode === 74; // Cmd + Option + J
+  const winLinuxShortcutJ = event.ctrlKey && event.shiftKey && event.keyCode === 74; // Ctrl + Shift + J
+  const chromeShortcut = event.ctrlKey && event.shiftKey && event.keyCode === 73; // Ctrl + Shift + I
+  const macShortcutC = event.metaKey && event.altKey && event.keyCode === 67; // Cmd + Option + C
+  const winLinuxShortcutC = event.ctrlKey && event.altKey && event.keyCode === 67; // Ctrl + Alt + C
+  const macNewShortcutC = event.metaKey && event.shiftKey && event.keyCode === 67; // Cmd + Shift + C
+  const winLinuxNewShortcutC = event.ctrlKey && event.shiftKey && event.keyCode === 67; // Ctrl + Shift + C
+  const macNewShortcutJ = event.metaKey && event.shiftKey && event.keyCode === 74; // Cmd + Shift + J
+  const winLinuxNewShortcutJ = event.ctrlKey && event.shiftKey && event.keyCode === 74; // Ctrl + Shift + J
+
+  if (isF12 || macShortcutJ || winLinuxShortcutJ || chromeShortcut || macShortcutC || winLinuxShortcutC || macNewShortcutC || winLinuxNewShortcutC || macNewShortcutJ || winLinuxNewShortcutJ) {
+    event.preventDefault();
+    alert("Curieux de connaître ce qui se cache derrière notre espace membre ? Découvre tout cela dans nos formations avancées 😉");
+  }
+});
+
+/*---  FIN : BLOCAGE CLIC DROIT ----*/
+
+/*---  DÉBUT : REDIRECTION ET BLOCAGE EN CAS D'ÉCHEC DE PAIEMENT ----*/
+
 async function checkMembershipAndRedirect() {
   const exclusionPage = "/app/actualisation-coordonnees-bancaires"; // Chemin de la page à exclure
   const currentPagePath = window.location.pathname;
@@ -23,36 +55,6 @@ document.addEventListener("DOMContentLoaded", checkMembershipAndRedirect);
 
 /*---  FIN : REDIRECTION ET BLOCAGE EN CAS D'ÉCHEC DE PAIEMENT ----*/
 
-/*---  DÉBUT : BLOCAGE CLIC DROIT ----*/
-
-// Bloquer le clic droit
-/*document.addEventListener('contextmenu', function (event) {
-  event.preventDefault();
-  alert("Curieux de connaître ce qui se cache derrière notre espace membre ? Découvre tout cela dans nos formations avancées 😉");
-});
-
-// Bloquer les raccourcis clavier
-document.addEventListener('keydown', function (event) {
-  // Bloquer les raccourcis courants pour accéder au panneau de développement
-  const isF12 = event.keyCode === 123;
-  const macShortcutJ = event.metaKey && event.altKey && event.keyCode === 74; // Cmd + Option + J
-  const winLinuxShortcutJ = event.ctrlKey && event.shiftKey && event.keyCode === 74; // Ctrl + Shift + J
-  const chromeShortcut = event.ctrlKey && event.shiftKey && event.keyCode === 73; // Ctrl + Shift + I
-  const macShortcutC = event.metaKey && event.altKey && event.keyCode === 67; // Cmd + Option + C
-  const winLinuxShortcutC = event.ctrlKey && event.altKey && event.keyCode === 67; // Ctrl + Alt + C
-  const macNewShortcutC = event.metaKey && event.shiftKey && event.keyCode === 67; // Cmd + Shift + C
-  const winLinuxNewShortcutC = event.ctrlKey && event.shiftKey && event.keyCode === 67; // Ctrl + Shift + C
-  const macNewShortcutJ = event.metaKey && event.shiftKey && event.keyCode === 74; // Cmd + Shift + J
-  const winLinuxNewShortcutJ = event.ctrlKey && event.shiftKey && event.keyCode === 74; // Ctrl + Shift + J
-
-  if (isF12 || macShortcutJ || winLinuxShortcutJ || chromeShortcut || macShortcutC || winLinuxShortcutC || macNewShortcutC || winLinuxNewShortcutC || macNewShortcutJ || winLinuxNewShortcutJ) {
-    event.preventDefault();
-    alert("Curieux de connaître ce qui se cache derrière notre espace membre ? Découvre tout cela dans nos formations avancées 😉");
-  }
-});*/
-
-/*---  FIN : BLOCAGE CLIC DROIT ----*/
-
 /*---  DÉBUT : AFFICHAGE CRÉDIT TOTAL DANS LA NAVIGATION DU MEMBRE ----*/
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -74,6 +76,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /*---  FIN : AFFICHAGE CRÉDIT TOTAL DANS LA NAVIGATION DU MEMBRE ----*/
+
 /*---  DÉBUT : DÉVEROUILLAGE FORMATION EN FONCTION DU PLAN DANS LA NAVIGATION ----*/
 console.log('Le script full page est chargé');
 async function updateTabLinksAndHideElementsForSpecificPlans() {
@@ -162,5 +165,5 @@ document.addEventListener("DOMContentLoaded", () => {
     updateTabLinksAndHideElementsForSpecificPlans();
 });
 
-/*---  DÉBUT : REDIRECTION ET BLOCAGE EN CAS D'ÉCHEC DE PAIEMENT ----*/
+
 
