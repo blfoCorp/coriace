497e93dd39fefab4a36868e0e6c606ed88044cca
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-03-21 15:29:33 +0100

    Update fullWebsite.js

--- fullWebsite.js
+++ fullWebsite.js
@@ -372,7 +372,9 @@ async function updateTabLinksAndHideElementsForSpecificPlans() {
                 dataTabLink: 'wfPlusButton',
                 dataLockIcon: 'wfPlusLockIcn',
                 dataStartButton: 'wfPlusStartButton',
-                dataHideButton: 'wfPlusHideButton'
+                dataHideButton: 'wfPlusHideButton',
+                newHref: '/app/webflow-plus'
+                
             }
         ];
         
