a3b4811d3306b5bec39523c4ceb0dd0a30b56338
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-03-24 11:02:42 +0100

    Update dripcontent-wpdv.js

--- dripcontent-wpdv.js
+++ dripcontent-wpdv.js
@@ -86,6 +86,20 @@ window.addEventListener('load', function() {
       var courseTimeName2 = document.querySelectorAll('[data-drip-content="courseTimeName2"]');
       var echeancePayment2 = document.querySelectorAll('[data-drip-content="echeancePayment2"]');
       var echeancePayment3 = document.querySelectorAll('[data-drip-content="echeancePayment3"]');
+
+      if (hasWpdvPlan) {
+        // Masquer immédiatement pour ecomPlan
+        courseTimeLeftCard1.forEach(function(card) {
+          card.style.display = 'none';
+        });
+      } else if (hasPackPlan || hasMegaPackPlan) {
+        // Masquer après 30 jours pour packPlan et megaPackPlan
+        if (daysSinceStart >= daysForLevel2) { // Utilise daysForLevel2 qui est défini à 30 jours
+          courseTimeLeftCard1.forEach(function(card) {
+            card.style.display = 'none';
+          });
+        }
+      }
       
       // Fonction pour mettre à jour le contenu en fonction du plan
       var updateContent = function(priceText, buttonText, buttonHref, name1Text, name2Text, paymentTime2, paymentTime3) {
