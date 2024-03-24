61ef85e314de6e32aff55d6ff62cc630f9fdadaa
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-03-24 10:20:04 +0100

    Update dripcontent-wpdv.js

--- dripcontent-wpdv.js
+++ dripcontent-wpdv.js
@@ -83,6 +83,8 @@ window.addEventListener('load', function() {
       var courseTimeLeftButton = document.querySelectorAll('[data-drip-content="courseTimeLeftButton"]');
       var courseTimeName1 = document.querySelectorAll('[data-drip-content="courseTimeName1"]');
       var courseTimeName2 = document.querySelectorAll('[data-drip-content="courseTimeName2"]');
+      var echeancePayment2 = document.querySelectorAll('[data-drip-content="echeancePayment2"]');
+      var echeancePayment3 = document.querySelectorAll('[data-drip-content="echeancePayment3"]');
       
       // Fonction pour mettre à jour le contenu en fonction du plan
       var updateContent = function(priceText, buttonText, buttonHref, name1Text, name2Text) {
