eddb58c69f1197a1ff56b9746429f493f8e3d5f0
BLFO CORP <154796211+blfoCorp@users.noreply.github.com>
2024-04-02 14:19:18 +0200

    Create blocage-console.js

---
+++ blocage-console.js
@@ -0,0 +1,28 @@
+ /*---  DÉBUT : BLOCAGE CLIC DROIT ----*/
+// Bloquer le clic droit
+document.addEventListener('contextmenu', function (event) {
+ event.preventDefault();
+  alert("Curieux de connaître ce qui se cache derrière notre espace membre ? Découvre tout cela dans nos formations avancées 😉");
+});
+
+// Bloquer les raccourcis clavier
+document.addEventListener('keydown', function (event) {
+  // Bloquer les raccourcis courants pour accéder au panneau de développement
+  const isF12 = event.keyCode === 123;
+  const macShortcutJ = event.metaKey && event.altKey && event.keyCode === 74; // Cmd + Option + J
+  const winLinuxShortcutJ = event.ctrlKey && event.shiftKey && event.keyCode === 74; // Ctrl + Shift + J
+  const chromeShortcut = event.ctrlKey && event.shiftKey && event.keyCode === 73; // Ctrl + Shift + I
+  const macShortcutC = event.metaKey && event.altKey && event.keyCode === 67; // Cmd + Option + C
+  const winLinuxShortcutC = event.ctrlKey && event.altKey && event.keyCode === 67; // Ctrl + Alt + C
+  const macNewShortcutC = event.metaKey && event.shiftKey && event.keyCode === 67; // Cmd + Shift + C
+  const winLinuxNewShortcutC = event.ctrlKey && event.shiftKey && event.keyCode === 67; // Ctrl + Shift + C
+  const macNewShortcutJ = event.metaKey && event.shiftKey && event.keyCode === 74; // Cmd + Shift + J
+  const winLinuxNewShortcutJ = event.ctrlKey && event.shiftKey && event.keyCode === 74; // Ctrl + Shift + J
+
+  if (isF12 || macShortcutJ || winLinuxShortcutJ || chromeShortcut || macShortcutC || winLinuxShortcutC || macNewShortcutC || winLinuxNewShortcutC || macNewShortcutJ || winLinuxNewShortcutJ) {
+    event.preventDefault();
+    alert("Curieux de connaître ce qui se cache derrière notre espace membre ? Découvre tout cela dans nos formations avancées 😉");
+  }
+});
+
+/*---  FIN : BLOCAGE CLIC DROIT ----*/
